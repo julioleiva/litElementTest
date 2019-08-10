4aaed1f036e59faf1fc4db7dee8a4b6d1de706e4
leivadiazjulio@gmail.com <leivadiazjulio@gmail.com>
2019-08-10 14:09:11 +0200

    add conditional-render and list-render components

---
+++ src/conditional-render.js
@@ -0,0 +1,40 @@
+import { LitElement, html, css } from "lit-element";
+
+class ConditionalRender extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+        h1 {
+          color: red;
+        }
+      }
+    `;
+  }
+
+  static get properties() {
+    return {
+      visible: { type: Boolean }
+    };
+  }
+
+  constructor() {
+    super();
+    this.visible = true;
+  }
+
+  render() {
+    return html`
+      <h3>Componente de Renderizado condicional con operador ternario</h3>
+      ${this.visible
+        ? html`
+            <h1>Te veo</h1>
+          `
+        : html`
+            <h1>No te veo</h1>
+          `}
+    `;
+  }
+}
+
+customElements.define("conditional-render", ConditionalRender);
